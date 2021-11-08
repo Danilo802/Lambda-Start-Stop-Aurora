b5f476f098129837f9f5c2eb1638a11964c19db4
Danillo Lima <danilo802@gmail.com>
2021-11-07 23:12:03 -0300

    Ajustes testes

--- lambda_function.py
+++ lambda_function.py
@@ -8,79 +8,143 @@ import boto3
 
 
 instanciasiniciadas=""
-def get_cluster(teste="N"):
+def get_cluster():
+    valortag = "False"
     rds_client_get = boto3.client("rds",region_name="sa-east-1")
     responseget = rds_client_get.describe_db_clusters() 
     resposta = "Nenhuma instancia iniciada"   
     for info in responseget["DBClusters"]:
         clustername = info['DBClusterIdentifier']
-        statuscluster = get_status(clustername)
-        if statuscluster == "stopped" and teste == "N":
-            arn = info['DBClusterArn']
-            valortag = list_tags_for_resource(arn)
-            if valortag == "True":
-                resposta  = action_cluster(clustername)  
-        if teste=="S":
-                resposta  = action_cluster(clustername,"S")                   
-    return resposta
+        statuscluster = info["Status"]
+        arn = info['DBClusterArn']
+        if statuscluster == "stopped":         
+            rds_client_get = boto3.client("rds",region_name="sa-east-1")    
+            tags = rds_client_get.list_tags_for_resource(
+                ResourceName=arn,
+            )['TagList']
+            for tag in tags:
+                if tag["Key"] == "TAG_START_NAME":
+                    if tag["Value"] == "True":
+                        valortag = tag["Value"]
+        if clustername == "db-master-teste":
+            valortag == "True"
 
-
-def get_status(clusternameconsulta):
-    rds_client_get = boto3.client("rds",region_name="sa-east-1")
-    responseget = rds_client_get.describe_db_clusters()
-    for info in responseget["DBClusters"]:
-        clustername = info['DBClusterIdentifier']
-        if clustername == clusternameconsulta:
-            statuscluster = info['Status']
-    return statuscluster
-
-def get_arn(clusternameconsulta):
-    rds_client_get = boto3.client("rds",region_name="sa-east-1")
-    responseget = rds_client_get.describe_db_clusters()
-    for info in responseget["DBClusters"]:
-        clustername = info['DBClusterIdentifier']
-        if clustername == clusternameconsulta:
-            arn = info['DBClusterArn']
-    return arn
-
-def list_tags_for_resource(arn, teste = "N"):
-    if teste == "S":
-        return "True"
-    rds_client_get = boto3.client("rds",region_name="sa-east-1")    
-    tags = rds_client_get.list_tags_for_resource(
-        ResourceName=arn,
-    )['TagList']
-
-    for tag in tags:
-        if tag["Key"] == "TAG_START_NAME":
-            if tag["Value"] == "True":
-                valortag = tag["Value"]
-    return valortag
-
-
-def action_cluster(clustername, teste="N"): 
-    if teste == "S":
-        return(clustername)
-    else:
-        rds_client = boto3.client("rds",region_name="sa-east-1") 
-        response = rds_client.start_db_cluster(DBClusterIdentifier = clustername)
-        global instanciasiniciadas
-        instanciasiniciadas = instanciasiniciadas + " " + clustername
-        return response
-
-
-
+        if valortag == "True" and clustername != "db-master-teste":
+            rds_client = boto3.client("rds",region_name="sa-east-1") 
+            resposta = rds_client.start_db_cluster(DBClusterIdentifier = clustername)
+            global instanciasiniciadas
+            instanciasiniciadas = instanciasiniciadas + " " + clustername
+        return resposta           
 
 def main() -> None:
     get_cluster()
 
 def lambda_handler(event, context):  
     main()
-    return{"statuscode": 200, "body":json.dumps(""), "Instâncias Iniciadas: ": instanciasiniciadas}
+    if event == 'teste':
+        return "OK"
+    else:
+        return{"statuscode": 200, "body":json.dumps(""), "Instâncias Iniciadas: ": instanciasiniciadas}
 
 if __name__ == "__main__":
     start = timeit.default_timer()
     lambda_handler(None,None)
     end = timeit.default_timer()
     print("Time: %f" % (end - start))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def get_cluster(teste="N"):
+#     rds_client_get = boto3.client("rds",region_name="sa-east-1")
+#     responseget = rds_client_get.describe_db_clusters() 
+#     resposta = "Nenhuma instancia iniciada"   
+#     for info in responseget["DBClusters"]:
+#         clustername = info['DBClusterIdentifier']
+#         statuscluster = get_status(clustername)
+#         if statuscluster == "stopped" and teste == "N":
+#             arn = info['DBClusterArn']
+#             valortag = list_tags_for_resource(arn)
+#             if valortag == "True":
+#                 resposta  = action_cluster(clustername)  
+#         if teste=="S":
+#                 resposta  = action_cluster(clustername,"S")                   
+#     return resposta
+
+
+# def get_status(clusternameconsulta):
+#     rds_client_get = boto3.client("rds",region_name="sa-east-1")
+#     responseget = rds_client_get.describe_db_clusters()
+#     for info in responseget["DBClusters"]:
+#         clustername = info['DBClusterIdentifier']
+#         if clustername == clusternameconsulta:
+#             statuscluster = info['Status']
+#     return statuscluster
+
+# def get_arn(clusternameconsulta):
+#     rds_client_get = boto3.client("rds",region_name="sa-east-1")
+#     responseget = rds_client_get.describe_db_clusters()
+#     for info in responseget["DBClusters"]:
+#         clustername = info['DBClusterIdentifier']
+#         if clustername == clusternameconsulta:
+#             arn = info['DBClusterArn']
+#     return arn
+
+# def list_tags_for_resource(arn, teste = "N"):
+#     if teste == "S":
+#         return "True"
+#     rds_client_get = boto3.client("rds",region_name="sa-east-1")    
+#     tags = rds_client_get.list_tags_for_resource(
+#         ResourceName=arn,
+#     )['TagList']
+
+#     for tag in tags:
+#         if tag["Key"] == "TAG_START_NAME":
+#             if tag["Value"] == "True":
+#                 valortag = tag["Value"]
+#     return valortag
+
+
+# def action_cluster(clustername, teste="N"): 
+#     if teste == "S":
+#         return(clustername)
+#     else:
+#         rds_client = boto3.client("rds",region_name="sa-east-1") 
+#         response = rds_client.start_db_cluster(DBClusterIdentifier = clustername)
+#         global instanciasiniciadas
+#         instanciasiniciadas = instanciasiniciadas + " " + clustername
+#         return response
+
+
+
+
+# def main() -> None:
+#     get_cluster()
+
+# def lambda_handler(event, context):  
+#     main()
+#     return{"statuscode": 200, "body":json.dumps(""), "Instâncias Iniciadas: ": instanciasiniciadas}
+
+# if __name__ == "__main__":
+#     start = timeit.default_timer()
+#     lambda_handler(None,None)
+#     end = timeit.default_timer()
+#     print("Time: %f" % (end - start))
 
--- test_app.py
+++ test_app.py
@@ -1,43 +1,22 @@
-
-import unittest
-from lambda_function import action_cluster, list_tags_for_resource
 import boto3
+import unittest
+from lambda_function import lambda_handler
 import mock
 import os
 from moto import mock_rds2
 
-nomecluster = ""
-arn = ""
+
 @mock.patch.dict(os.environ,{'ENV': "mytemp"})
 @mock_rds2
 class TestActionCluster(unittest.TestCase):
     def test_action_cluster(self):
-        mock_lambda_handler()
-        global nomecluster
+        testar_lambda_handler()
         import lambda_function      
-        self.assertEqual(lambda_function.action_cluster(nomecluster,'S'),nomecluster)
-    
-    def test_get_arn(self):
-        global arn
-        import lambda_function      
-        self.assertEqual(lambda_function.get_arn(nomecluster),arn)
-
-    def test_get_status(self):
-        global nomecluster
-        import lambda_function      
-        self.assertEqual(lambda_function.get_status(nomecluster),"available")
-
-    def test_list_tags_for_resource(self):
-        global arn
-        import lambda_function      
-        self.assertEqual(lambda_function.list_tags_for_resource(arn, "S"),"True")
-
-    def test_get_cluster(self): 
-        import lambda_function      
-        self.assertEqual(lambda_function.get_cluster("S"),"db-master-1")
+        self.assertEqual(lambda_function.lambda_handler('teste',''),"OK")
 
 
-def mock_lambda_handler():
+
+def testar_lambda_handler():
     client = boto3.client('rds',region_name='sa-east-1')
     client.create_db_cluster(
         BackupRetentionPeriod=1,
@@ -54,30 +33,107 @@ def mock_lambda_handler():
         Engine='aurora-mysql',
         KmsKeyId="arn:teste",
         MasterUsername ="root",
-        MasterUserPassword='hunter2',
+        MasterUserPassword='hunter2teste',
         PreferredBackupWindow="01:00-03:00",
         PreferredMaintenanceWindow="sun:06:00-sun:07:00 UTC",
         StorageEncrypted=False,
         VpcSecurityGroupIds= ["454s8d8de"],
-        DBClusterIdentifier='db-master-1',
+        DBClusterIdentifier='db-master-teste',
         DatabaseName='staging-aurora',
         Port=8085
     )
-    rds_client_get = boto3.client("rds",region_name="sa-east-1")
-    responseget = rds_client_get.describe_db_clusters()    
-    global nomecluster 
-    global arn
-    for info in responseget["DBClusters"]:
-        nomecluster = info['DBClusterIdentifier']
-        arn = info['DBClusterArn'] 
+
     with mock.patch("boto3.client") as mock_client:
         mock_client.return_value = client
         print(mock_client.return_value)
 
 
 
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
+
+
+
+# import unittest
+# from lambda_function import action_cluster, list_tags_for_resource
+# import boto3
+# import mock
+# import os
+# from moto import mock_rds2
+
+
+# nomecluster = ""
+# arn = ""
+# @mock.patch.dict(os.environ,{'ENV': "mytemp"})
+# @mock_rds2
+# class TestActionCluster(unittest.TestCase):
+#     def test_action_cluster(self):
+#         mock_lambda_handler()
+#         global nomecluster
+#         import lambda_function      
+#         self.assertEqual(lambda_function.action_cluster(nomecluster,'S'),nomecluster)
+    
+#     def test_get_arn(self):
+#         global arn
+#         import lambda_function      
+#         self.assertEqual(lambda_function.get_arn(nomecluster),arn)
+
+#     def test_get_status(self):
+#         global nomecluster
+#         import lambda_function      
+#         self.assertEqual(lambda_function.get_status(nomecluster),"available")
+
+#     def test_list_tags_for_resource(self):
+#         global arn
+#         import lambda_function      
+#         self.assertEqual(lambda_function.list_tags_for_resource(arn, "S"),"True")
+
+#     def test_get_cluster(self): 
+#         import lambda_function      
+#         self.assertEqual(lambda_function.get_cluster("S"),"db-master-1")
+
+
+# def mock_lambda_handler():
+#     client = boto3.client('rds',region_name='sa-east-1')
+#     client.create_db_cluster(
+#         BackupRetentionPeriod=1,
+#         CharacterSetName="profilex",
+#         OptionGroupName="profilex",
+#         AvailabilityZones=["sa-east-1b"],
+#         Tags=[{'Key': "testes",'Value': "testes"}],        
+#         CopyTagsToSnapshot=False,
+#         DBClusterParameterGroupName='default.aurora',
+#         DBSubnetGroupName='profilex',
+#         DeletionProtection=False,
+#         EnableIAMDatabaseAuthentication=False,
+#         EngineVersion='5.7.mysql_aurora.2.10.0',
+#         Engine='aurora-mysql',
+#         KmsKeyId="arn:teste",
+#         MasterUsername ="root",
+#         MasterUserPassword='hunter2',
+#         PreferredBackupWindow="01:00-03:00",
+#         PreferredMaintenanceWindow="sun:06:00-sun:07:00 UTC",
+#         StorageEncrypted=False,
+#         VpcSecurityGroupIds= ["454s8d8de"],
+#         DBClusterIdentifier='db-master-1',
+#         DatabaseName='staging-aurora',
+#         Port=8085
+#     )
+#     rds_client_get = boto3.client("rds",region_name="sa-east-1")
+#     responseget = rds_client_get.describe_db_clusters()    
+#     global nomecluster 
+#     global arn
+#     for info in responseget["DBClusters"]:
+#         nomecluster = info['DBClusterIdentifier']
+#         arn = info['DBClusterArn'] 
+#     with mock.patch("boto3.client") as mock_client:
+#         mock_client.return_value = client
+#         print(mock_client.return_value)
+
+
+
+# # if __name__ == "__main__":
+# #     unittest.main()
 
 
 
